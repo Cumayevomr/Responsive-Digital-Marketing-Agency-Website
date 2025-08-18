50c626cd78ed22120980973c85ecf8040c50d03a
Cumayevomr <cumayevomr8@gmail.com>
2025-08-18 16:28:15 +0400

    @media banner css

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // Header Scroll
 
 let nav = document.querySelector(".navbar");
-window.onScroll = function(){
+window.onscroll = function(){
     if(document.documentElement.scrollTop > 20){
         nav.classList.add("header-scrolled");
     } else {
